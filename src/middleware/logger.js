9421f3a6262f9c046263efadccd010f43062ff97
Mert ÜNAL <mertunal1998@gmail.com>
2026-09-20 18:48:13 +0300

    logger body added

--- src/middleware/logger.js
+++ src/middleware/logger.js
@@ -1,13 +1,18 @@
 const logger = (req, res, next) => {
   const now = new Date();
+
   const timestamp =
     `${now.getDate()}.${now.getMonth() + 1}.${now.getFullYear()} ` +
     `${now.getHours()}:${now.getMinutes()}:${now.getSeconds()}`;
 
   const method = req.method;
   const url = req.originalUrl;
-  
+
   console.log(`${method} ${url} || ${timestamp}`);
+
+   if(req.body != null){
+    console.log("Body:",req.body);
+   }
 
   next();
 };
